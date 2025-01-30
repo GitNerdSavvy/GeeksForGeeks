a1a54a3e54a26118590107728d18ddf45ae38ac2
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-01-30 10:40:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int large=-1;
+        int ans=-1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>large){
+                ans=large;
+                large=arr[i];
+            }
+            else if(arr[i]!=large){
+                if(ans==-1 || arr[i]>ans){
+                    ans=arr[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSecondLargest(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
