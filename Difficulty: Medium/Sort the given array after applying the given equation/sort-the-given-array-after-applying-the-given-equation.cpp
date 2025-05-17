37640fd8309cc82740c60d790248ce233d0bad13
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2025-05-17 20:12:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort the given array after applying the given equation/sort-the-given-array-after-applying-the-given-equation.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> sortArray(vector<int> &arr, int A, int B, int C) {
+        // code here
+        vector<int>ans;
+        multiset<int>s;
+        for (int i = 0; i < arr.size(); i++) {
+        s.insert(A * arr[i] * arr[i] + B * arr[i] + C);
+        }
+        for(auto i=s.begin();i!=s.end();++i){
+            ans.push_back(*i);
+        }
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int a, b, c;
+        cin >> a >> b >> c;
+        cin.ignore();
+
+        Solution obj;
+        vector<int> ans = obj.sortArray(arr, a, b, c);
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << ' ';
+        cout << endl;
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
