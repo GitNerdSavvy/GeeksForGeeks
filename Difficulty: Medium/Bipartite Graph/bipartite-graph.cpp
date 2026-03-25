814dd405e1df60861f7758be2c94e02f38a85c17
Sanjeev yadav <136702091+GitNerdSavvy@users.noreply.github.com>
2026-03-25 16:43:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    bool isBipartite(int V, vector<vector<int>> &edges) {
+        // Code here
+        vector<int>adj[V];
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<int>color(V,-1);
+        queue<int>q;
+       for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                queue<int> q;
+                q.push(i);
+                color[i] = 0;
+
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    for (auto neighbor : adj[node]) {
+                        if (color[neighbor] == -1) {
+                            color[neighbor] = 1 - color[node];
+                            q.push(neighbor);
+                        } else if (color[neighbor] == color[node]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
